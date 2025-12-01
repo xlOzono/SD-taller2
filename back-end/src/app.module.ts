8e0efe78bc32d09b6ae1d9f334d28f39a919bc71
UnderWater <131457895+FabianQuezada@users.noreply.github.com>
2025-12-01 20:28:18 -0300

    creacion del back

---
+++ back-end/src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsuarioModule } from './usuario/usuario.module';
+
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT ?? '3306', 10),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      entities: [User],
+      synchronize: false, // o true mientras pruebas el modelo
+    }),
+    UsersModule,
+    UsuarioModule,
+  ],
+})
+export class AppModule {}
